0be0a93c01edea46c7788270059824f951c1706a
3TCorp <3tcorp.vn@gmail.com>
2026-01-29 21:37:40 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace eidca.sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            token_challenge = new TextBox();
+            lblToken = new Label();
+            txtChallenge = new TextBox();
+            button1 = new Button();
+            signature = new TextBox();
+            button2 = new Button();
+            lblFullName = new Label();
+            picAvatar = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)picAvatar).BeginInit();
+            SuspendLayout();
+            // 
+            // token_challenge
+            // 
+            token_challenge.Location = new Point(12, 32);
+            token_challenge.Multiline = true;
+            token_challenge.Name = "token_challenge";
+            token_challenge.Size = new Size(483, 74);
+            token_challenge.TabIndex = 0;
+            token_challenge.Text = resources.GetString("token_challenge.Text");
+            // 
+            // lblToken
+            // 
+            lblToken.AutoSize = true;
+            lblToken.Location = new Point(13, 13);
+            lblToken.Name = "lblToken";
+            lblToken.Size = new Size(125, 15);
+            lblToken.TabIndex = 1;
+            lblToken.Text = "token_challenge (JWT)";
+            // 
+            // txtChallenge
+            // 
+            txtChallenge.Location = new Point(230, 126);
+            txtChallenge.Name = "txtChallenge";
+            txtChallenge.Size = new Size(265, 23);
+            txtChallenge.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(13, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(211, 23);
+            button1.TabIndex = 3;
+            button1.Text = "2. Giải mã lấy Challenge";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // signature
+            // 
+            signature.Location = new Point(231, 169);
+            signature.Multiline = true;
+            signature.Name = "signature";
+            signature.Size = new Size(264, 118);
+            signature.TabIndex = 4;
+            signature.TextChanged += signature_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(13, 168);
+            button2.Name = "button2";
+            button2.Size = new Size(211, 23);
+            button2.TabIndex = 5;
+            button2.Text = "3. Gửi căn cước Ký chuỗi challenge";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // lblFullName
+            // 
+            lblFullName.AutoSize = true;
+            lblFullName.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblFullName.Location = new Point(550, 76);
+            lblFullName.Name = "lblFullName";
+            lblFullName.Size = new Size(68, 15);
+            lblFullName.TabIndex = 6;
+            lblFullName.Text = "HỌ VÀ TÊN";
+            // 
+            // picAvatar
+            // 
+            picAvatar.Location = new Point(550, 104);
+            picAvatar.Name = "picAvatar";
+            picAvatar.Size = new Size(159, 183);
+            picAvatar.TabIndex = 7;
+            picAvatar.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 307);
+            label1.Name = "label1";
+            label1.Size = new Size(408, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Bước 1: Đọc căn cước - Bước 2: Giải mã lấy challenge - Bước 3:  Ký challenge";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(550, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 15);
+            label2.TabIndex = 9;
+            label2.Text = "1. Đọc thẻ căn cước";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(756, 335);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(picAvatar);
+            Controls.Add(lblFullName);
+            Controls.Add(button2);
+            Controls.Add(signature);
+            Controls.Add(button1);
+            Controls.Add(txtChallenge);
+            Controls.Add(lblToken);
+            Controls.Add(token_challenge);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)picAvatar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox token_challenge;
+        private Label lblToken;
+        private TextBox txtChallenge;
+        private Button button1;
+        private TextBox signature;
+        private Button button2;
+        private Label lblFullName;
+        private PictureBox picAvatar;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Logging;
+using NextBiometrics.Devices;
+using RAR.IdCard.Sdk.Reader;
+using RAR.IdCard.Sdk.Reader.HN212;
+namespace eidca.sample
+{
+    public partial class Form1 : Form
+    {
+        private VnHn212Reader _reader = new VnHn212Reader();
+        public Form1()
+        {
+            InitializeComponent();
+            InitReader();
+        }
+        private void InitReader()
+        {
+            /*Register all reader status event*/
+            _reader.OnStatusChanged += Reader_OnStatusChanged;
+
+            // var config = new VnPcscsConfig();
+            var config = new VnHn212Config();
+
+            /* set this to false: input access code or scan & read as required
+             * true: Auto scan card & read data*/
+            // config.AutoReadWhenPresent = false;
+
+            /*Set this flag to process VerifySOD*/
+            config.DoVerifySOD = true;
+
+            /*Set this flag to process AA/CA Authen*/
+            config.CheckAACAAuthen = true;
+
+            /* Read DG2 */
+            config.DoReadDg2Face = true;
+
+            /* Capture face, Minimum time have real face*/
+            /* Default is 200ms */
+            config.AutoCaptureFaceMinimumMs = 200;
+
+            /* Retry count */
+            config.ReadCardRetryCount = 5;
+
+            /* Seq */
+            //config.ProcessSequence = HN212ProSeq.CardOnly;
+            config.ProcessSequence = HN212ProSeq.CardThenFace;
+
+            /* Use an external camera that is not plugged directly into the HN212-Reader */
+            config.CaptureFaceUseFirstCamAvaiable = true;
+            config.AutoCaptureFaceAntiSpoofing = true;
+            config.CaptureFaceAllowMultiFace = true;
+            config.CaptureFaceMinFaceWidth = 300;
+
+            config.AutoCaptureFaceEnabled = true;
+            config.CaptureFaceDetectFace = true;
+            config.CaptureFaceDisplayTimeout = true;
+            config.CaptureFaceTimeout = 200;
+
+            config.HidLibUsed = HN212HidLibrary.HidApi;
+
+            config.internalDecodeQrcodeType = InternalDecodeQrcodeType.DECODE_ONCE;
+            config.AllowExtAa = true;
+            /*Start reader monitoring ...*/
+            _reader.StartMonitor(config);
+
+            _reader.FaceAntiSpoofMode = HN212SpoofMode.MID;
+            _reader.OnVideoFrame += reader_OnVideoFrame;
+
+        }
+
+        private void reader_OnVideoFrame(object sender, StatusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Reader_OnStatusChanged(object sender, StatusEventArgs e)
+        {
+            //Call from other thread
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    OnEvent(sender, e);
+                }));
+            }
+            else
+            {
+                OnEvent(sender, e);
+            }
+        }
+        private void OnEvent(object sender, StatusEventArgs e)
+        {
+            try
+            {
+
+
+                switch (e.EventName)
+                {
+                    case EVENT_NAMES.READER:
+                        ProcessReaderEvent(e as StatusEventReaderArgs);
+                        break;
+                    case EVENT_NAMES.CARD:
+                        ProcessCardEvent(e as StatusEventCardArgs);
+                        break;
+                    case EVENT_NAMES.READ:
+                        ProcessReadEvent(e as StatusEventReadArgs);
+                        break;
+                    case EVENT_NAMES.READER_CAMERA:
+                        ProcessCameraEvent(e as StatusEventCameraArgs);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log.Error("Error", ex);
+            }
+        }
+
+        private void ProcessReaderEvent(StatusEventReaderArgs? ev)
+        {
+            if (ev == null)
+                return;
+
+
+        }
+
+        private void ProcessCameraEvent(StatusEventCameraArgs? ev)
+        {
+            if (ev == null)
+                return;
+        }
+
+        private void ProcessCardEvent(StatusEventCardArgs? ev)
+        {
+            if (ev == null)
+                return;
+            //Log.Debug($"----> [{DateTime.Now.ToString("HH:mm:ss")}] Card in Device {ev.ReaderSerialNumber} change stated from: {ev.LastState} to: {ev.NewState}.");
+        }
+        private void ProcessReadEvent(StatusEventReadArgs? ev)
+        {
+            try
+            {
+                if (ev == null)
+                    return;
+                //Log.Debug($"----> [{DateTime.Now.ToString("HH:mm:ss")}] Device {ev.ReaderSerialNumber} do step: {ev.Step}; status: {ev.Status}; message={ev.Message}.");
+                switch (ev.Step)
+                {
+                    case READ_CARD_STEPS.SCANCARD:
+                    case READ_CARD_STEPS.START:
+                    case READ_CARD_STEPS.CONNECT_CARD:
+                    case READ_CARD_STEPS.PACE:
+                    case READ_CARD_STEPS.READ_DGS:
+                    case READ_CARD_STEPS.VERIFY_SOD:
+                    case READ_CARD_STEPS.AACA_AUTHEN:
+                        break;
+                    case READ_CARD_STEPS.FINISH:
+                        OnReadCardFinish(ev);
+                        break;
+                }
+            }
+            catch (Exception exc)
+            {
+                // Log.Error("Error", exc);
+            }
+        }
+
+        private void OnReadCardFinish(StatusEventReadArgs ev)
+        {
+
+            if (_reader.CardData.Dg2File != null && !string.IsNullOrEmpty(_reader.CardData.Dg2File.FaceImage))
+            {
+                lblFullName.Text = _reader.CardData.Dg13File?.Name;
+                LoadImageToPictureBox(_reader.CardData.Dg2File.FaceImage, picAvatar);              
+
+               
+            }
+        }
+        private void UnInitReader()
+        {
+            _reader.OnVideoFrame -= reader_OnVideoFrame;
+            _reader.OnStatusChanged -= Reader_OnStatusChanged;
+            _reader.StopMonitor();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtChallenge.Text = DecodeJwtToken(token_challenge.Text);
+        }
+
+        public string DecodeJwtToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            // Kiểm tra token có hợp lệ không
+            if (!handler.CanReadToken(token))
+            {
+                throw new ArgumentException("Token không hợp lệ");
+            }
+
+            // Chỉ giải mã mà không xác thực (cho mục đích đọc thông tin)
+            var jwtToken = handler.ReadJwtToken(token);
+
+            // Tìm claim có Type = "type" và Value = "challenge"
+            var typeClaim = jwtToken.Claims
+                .FirstOrDefault(c => c.Type == "challenge");
+
+            return typeClaim?.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var rst = _reader.StartAA(Convert.FromBase64String(txtChallenge.Text));
+            
+            
+            signature.Text = Convert.ToBase64String(rst.Signature);
+            
+        }
+
+        private void signature_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UnInitReader();
+        }
+
+        public static Bitmap Base64ToBitmap(string base64String)
+        {
+            try
+            {
+                // Loại bỏ data URI scheme nếu có
+                string base64Data = CleanBase64String(base64String);
+
+                byte[] imageBytes = Convert.FromBase64String(base64Data);
+
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    return new Bitmap(ms);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi tải hình ảnh: {ex.Message}", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        private static string CleanBase64String(string base64String)
+        {
+            // Loại bỏ data URI scheme (data:image/png;base64,)
+            if (base64String.Contains("base64,"))
+            {
+                return base64String.Split(',')[1];
+            }
+            return base64String;
+        }
+
+        public static void LoadImageToPictureBox(string base64String, PictureBox pictureBox)
+        {
+            try
+            {
+                var bitmap = Base64ToBitmap(base64String);
+                if (bitmap != null)
+                {
+                    pictureBox.Image?.Dispose(); // Giải phóng hình cũ
+                    pictureBox.Image = bitmap;
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom; // Tự động scale
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi tải hình ảnh: {ex.Message}");
+            }
+        }
+
+    }
+}
